55b41a9bd9fc62959b8eeebb8890cbb8e3be9fdc
omayma ahmad <omayma.ahmad25@gmail.com>
2020-11-15 16:26:48 +0000

    added functions for checkAnswer and EndQuiz

--- script.js
+++ script.js
@@ -1,64 +1,104 @@
+var questions = [
+  {
+    question: "'Snow White' is an example of a ___?",
+    answers: ["boolean", "number", "string"],
+    correct: "string",
+  },
+  {
+    question: "What is an array?",
+    answers: ["list of strings", "list of values", "list of returns"],
+    correct: "list of values",
+  },
+  {
+    question: "An iterator tells us how many times we will ___?",
+    answers: ["log", "loop", "dance"],
+    correct: "loop",
+  },
+  {
+    question: "Properties do not have ___?",
+    answers: ["furniture", "a string", "an order"],
+    correct: "an order",
+  },
+  {
+    question: "What is an object?",
+    answers: ["collection of key value pairs", "a noun", "multiple strings"],
+    correct: "collection of key value pairs",
+  },
+];
+var currentQuestionIndex = 0;
 
-// Homework Code //
 
-// function beginBtn() {
-    //     var myVar = setInterval(myTimer, 1000);
-    
-    //     function myTimer() {
-        
-        //     }
-        // }
-        
-        /*Variables needed for the code execution
-        - Questions which are going to be asked (array of objects which means you iterate over them and display): var questions = [{}, {}, {}]
-        - going to need to get all of the different sections in html: var startingPage = document.getElementById("startScreen");
-        - going to need a variable to reference the current index question 
-        */
-       var questions = [{
-           question: "Whats your name?",
-           answers: ["Bob", "Bill", "bert"],
-           correct: "Bob"
-       }]
-       var currentQuestionIndex = 0; 
-       
 var beginBtn = document.getElementById("begin");
 
+function startQuiz() {
+  var startScreenContent = document.getElementById("startScreen");
+  startScreenContent.style.display = "none";
+  //start the timer
 
-function startQuiz(){
-    var startScreenContent = document.getElementById("startScreen");
-    startScreenContent.style.display = "none";
-    //start the timer
-    //then you call another function which renders the questions onto the screen: showQuestions(); 
-showQuestions();
+  showQuestions();
 }
 
-function showQuestions(){
- var currentQuestion = document.getElementById("currentQuestion");
- currentQuestion.textContent = questions[currentQuestionIndex].question
+function showQuestions() {
+  var currentQuestion = document.getElementById("currentQuestion");
+  currentQuestion.textContent = questions[currentQuestionIndex].question;
 
- var answersSection = document.getElementById("answersSection"); 
- for (var i = 0; i < questions[currentQuestionIndex].answers.length; i++){
-     var answerButton = document.createElement("button"); 
-     answerButton.textContent = questions[currentQuestionIndex].answers[i];
-     answerButton.onclick = checkAnswer();
+  var answersSection = document.getElementById("answersSection");
+  answersSection.innerHTML = "";
+  for (var i = 0; i < questions[currentQuestionIndex].answers.length; i++) {
+    var answerButton = document.createElement("button");
+    answerButton.textContent = questions[currentQuestionIndex].answers[i];
+    answerButton.setAttribute(
+      "value",
+      questions[currentQuestionIndex].answers[i]
+    );
+    answerButton.onclick = checkAnswer;
     answersSection.appendChild(answerButton);
-    }
+  }
 }
 
-function checkAnswer(){
-//look at a way where we can use the 'this' keyword to check whats been clicked against the answer to the question at current index 
-//if correct do something
-//if wrong do something
-// increment the current index and call the showQuestions function again
-//unless there are no questions left (can check the length of the array against the current index)....when we end the quiz by calling a function to end the quiz
+function checkAnswer() {
+  var answerSelected = this.value;
+  console.log(answerSelected);
+  if (answerSelected === questions[currentQuestionIndex].correct) {
+    alert("correct");
+  } else {
+    alert("Incorrect");
+  }
+  currentQuestionIndex++;
+  if (currentQuestionIndex === questions.length) {
+    endQuiz();
+  } else {
+    showQuestions();
+  }
+
+  //look at a way where we can use the 'this' keyword to check whats been clicked against the answer to the question at current index
+  //if correct do something
+  //if wrong do something
+  // increment the current index and call the showQuestions function again
+  //unless there are no questions left (can check the length of the array against the current index)....when we end the quiz by calling a function to end the quiz
 }
 
-function endQuiz(){
-    //display score etc
+// Count highscore
+function addPersonToList(event) {
+  event.preventDefault();
+  var name = nameEl.value;
+  var score = scoreEl.value;
+  var li = document.createElement("li");
+  li.id = peopleScore.length;
+  li.innerHTML = name + score + " <button>edit</button>";
+  peopleScore.push({ name: score });
+  peopleScoreListEl.append(li);
+}
+
+function endQuiz() {
+  var endScore = checkAnswer;
+  var questionsArea = document.getElementById("questions");
+  questionsArea.style.display = "none"; 
+  var finalMessage = document.getElementById("thanksMessage");
+  finalMessage.textContent = "Thanks for taking part!"
+  //display score etc
 }
 
 beginBtn.onclick = startQuiz;
 
-
 // Bottom of the page initise the start of the code execution startQuiz();
-
